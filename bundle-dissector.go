5336b1f8c56c9715a8f36dc9749c7dd3e00d1122
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-10-13 19:11:57 +0200

    wip: setting up http stream

--- bundle-dissector.go
+++ bundle-dissector.go
@@ -6,10 +6,12 @@ import "net/http"
 import "github.com/vladimirvivien/automi/stream"
 
 func main() {
+	fmt.Println("hello world!")
 	node := "http://gateway-2-temp.arweave.net:1984"
-	http.HandleFunc(
+	http.NewRequest("GET",
 		node + "/tx/" + "VSkuqLKJiA9vIHA_38cg3zMdFVIe7aaAUCJXLKuPFFU",
 		func(resp http.ResponseWriter, req *http.Request) {
+			fmt.Println("hello http")
 			strm := stream.New(req.Body)
 
 			sink := new(bytes.Buffer)
